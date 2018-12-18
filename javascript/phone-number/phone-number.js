7c1befeade44b586b4640ff6e8a007971450710b
Hong Jeesoo <jeesoo.hong@gmail.com>
2018-12-18 23:53:08 +0900

    정규표현식을 사용한 phone-number 풀이

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,1 +1,12 @@
-000 - 000 - 0000;
+export default class PhoneNumber {
+  constructor(enteredNum) {
+    this.enteredNum = enteredNum;
+  }
+
+  number() {
+    const result = this.enteredNum
+      .trim()
+      .match(/^\+?1?\D*([2-9]\d{2})\D*([2-9]\d{2})\D*(\d{4})$/);
+    return result && result[1] + result[2] + result[3];
+  }
+}
